02ce96e61eb8cf4e8736a66e4c899858d3005981
yuanwen <wenshin2011@gmail.com>
2016-05-15 11:11:21 +0800

    Update: 完善测试用例并修复部分问题

--- test/index.js
+++ test/index.js
@@ -6,17 +6,23 @@ let promiseAllEnd = require('../lib');
 let PROMISE_DELAY = 5;
 let errorPromise = Promise.reject('error');
 
+/**
+ * 测试用例
+ * 1. promise.then(f)
+ * 2. promise.then(f).then(ff)
+ * 3. promise.then(f).catch(r)
+ * 4. promise.then(f, r).then(ff)
+ * 5. promise.catch(r)
+ */
+
 describe('', function () {
   it('输入为全部成功的 promise 数组，应该返回数组数据，且没有错误', function (done) {
-    let result = {};
+    let data;
     promiseAllEnd([Promise.resolve(1), Promise.resolve(2)])
-      .then( (data, error) => {
-        result = {data, error};
-      });
+      .then(_data => data = _data);
 
     setTimeout(() => {
-      assert.deepEqual(result.data, [1, 2], `全部成功返回所有数据。${result.data}`);
-      assert.ok(!result.error, '全部成功，错误为空')
+      assert.deepEqual(data, [1, 2], `全部成功返回所有数据。${data}`);
       done();
     }, PROMISE_DELAY)
   });
@@ -61,12 +67,26 @@ describe('', function () {
     }, PROMISE_DELAY)
   });
 
-  it('输入为全部失败的 promise 对象，应该只触发 catch', function (done) {
+  it('输入为部分成功的 promise 对象，promise.then().then() 调用正常', function (done) {
+    let data;
+    let data1;
+    promiseAllEnd({key1: Promise.resolve(1), key2: errorPromise, key3: Promise.resolve(3)})
+      .then(_data => {data = _data; return 1;})
+      .then(_data => data1 = _data)
+
+    setTimeout(() => {
+      assert.deepEqual(data, {key1: 1, key3: 3}, `promise.then().then() 二级调用正常。${data}`);
+      assert.deepEqual(data1, 1, `promise.then().then() 二级调用正常。${data1}`);
+      done();
+    }, PROMISE_DELAY)
+  });
+
+  it('输入为全部失败的 promise 对象，应该只触发一次错误', function (done) {
     let data = null;
     let error = null;
     let error2 = null;
     promiseAllEnd([errorPromise, errorPromise])
-      .then(_data => data = _data, _error => {error = _error})
+      .then(_data => data = _data, _error => error = _error)
       .catch(_error => error2 = _error);
 
     setTimeout(() => {
